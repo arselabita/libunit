ad0c24d34dc19120706f30513de58e320982694d
Arsela Bita <arselabita@gmail.com>
2025-07-05 13:52:41 +0200

    fixed header

--- tests/strlen/00_launcher.c
+++ tests/strlen/00_launcher.c
@@ -2,7 +2,7 @@
     A function to load all the test that you want to run for a specific function.
     This is the first file to be compiled, so the main entry point for this test gorup.
 */
-#include "libunit.h"
+#include "../../framework/libunit.h"
 
 int strlen_launcher(void)
 {
